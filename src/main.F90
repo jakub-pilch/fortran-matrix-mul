6e899ce0fec34453395ccd51f5613bab6e047cb5
FizzyJelly <jakp1101@gmail.com>
2019-04-26 19:10:06 +0200

    Added naive implementations for kind=8 and kind=16. Tested correctness of multiplication

--- src/main.F90
+++ src/main.F90
@@ -1,6 +1,26 @@
 program main
-implicit none
+    use naivemath
+    implicit none
 
+
+
+    real(kind=8),dimension(3,2) :: A
+    real(kind=8),dimension(2,3) :: B
+    real(kind=8),dimension(3,3) :: C
+
+    integer :: i,j
+
+    do i=1,3
+        do j=1,2
+            A(i,j)=i
+            B(j,i)=j
+        end do
+    end do
+
+    C=mull(A,B)  
+    write(*,*) C
+    write(*,*) size(C,dim=1)
+    write(*,*) size(C,dim=2)
 
 
 end program main
--- src/naivemath.F90
+++ src/naivemath.F90
@@ -27,6 +27,50 @@ module naivemath
         do i=1, size(A,dim=1)
             do j=1, size(B,dim=2)
                 do k=1,size(A,dim=2)
+                    C(i,j) = C(i,j) + A(i,k)*B(k,j)
+                end do
+            end do
+        end do
+        
+    end function
+
+    function mull_8(A,B) result(C)
+        implicit none
+        real (kind=8), intent(in), dimension(:,:) :: A,B
+        real (kind=8), allocatable, dimension(:,:) :: C
+        integer :: i,j,k
+
+        if(size(A,dim=2) /= size(B,dim=1)) then ! mat dimension n x m * m x p => n x p 
+            stop "Matrix size does not match!"
+        end if
+
+        allocate(C(size(A,dim=1),size(B,dim=2)))
+
+        do i=1, size(A,dim=1)
+            do j=1, size(B,dim=2)
+                do k=1,size(A,dim=2)
+                    C(i,j) = C(i,j) + A(i,k)*B(i,k)
+                end do
+            end do
+        end do
+        
+    end function
+
+    function mull_16(A,B) result(C)
+        implicit none
+        real (kind=16), intent(in), dimension(:,:) :: A,B
+        real (kind=16), allocatable, dimension(:,:) :: C
+        integer :: i,j,k
+
+        if(size(A,dim=2) /= size(B,dim=1)) then ! mat dimension n x m * m x p => n x p 
+            stop "Matrix size does not match!"
+        end if
+
+        allocate(C(size(A,dim=1),size(B,dim=2)))
+
+        do i=1, size(A,dim=1)
+            do j=1, size(B,dim=2)
+                do k=1,size(A,dim=2)
                     C(i,j) = C(i,j) + A(i,k)*B(i,k)
                 end do
             end do
